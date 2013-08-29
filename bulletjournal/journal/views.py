1ce32936fb029c698fe7b3c19bba4bd762de26b1
Marcos Gabriel Alcazar <marcos.alcazar@gmail.com>
2013-08-29 16:34:12 -0300

    License in _base.html. Comments on journal.views. Moved generic JSON view to bullethournal.views

--- bulletjournal/journal/views.py
+++ bulletjournal/journal/views.py
@@ -1,37 +1,11 @@
+from bulletjournal.views import JSONResponseMixin
 from collections import OrderedDict
-from django.http import HttpResponse
+from django.utils import simplejson
 from django.views.generic.base import TemplateView
-from journal.models import Event, Task, Item, CompletableItem
+from django.views.generic.edit import BaseUpdateView
+from journal.models import Event, Task, CompletableItem
 import calendar
 import datetime
-import json
-from django.views.generic.edit import UpdateView, BaseUpdateView
-from django.shortcuts import render_to_response
-from django.utils import simplejson
-
-
-
-class JSONResponseMixin(object):
-    """
-    A mixin that can be used to render a JSON response.
-    """
-    def render_to_json_response(self, context, **response_kwargs):
-        """
-        Returns a JSON response, transforming 'context' to make the payload.
-        """
-        return HttpResponse(
-            self.convert_context_to_json(context),
-            content_type='application/json',
-            **response_kwargs
-        )
-
-    def convert_context_to_json(self, context):
-        "Convert the context dictionary into a JSON object"
-        # Note: This is *EXTREMELY* naive; in reality, you'll need
-        # to do much more complex handling to ensure that arbitrary
-        # objects -- such as Django model instances or querysets
-        # -- can be serialized as JSON.
-        return json.dumps(context)
 
 
 class MonthView(TemplateView):
@@ -40,20 +14,34 @@ class MonthView(TemplateView):
     
     def get_context_data(self, **kwargs):
         c = TemplateView.get_context_data(self, **kwargs)
+        
         today = datetime.date.today()
-        c['today'] = today
+        c['today'] = today # Added to the context so we can use it in the template
+        
+        # Query all tasks for the user, only parents, for this month
         c['tasks'] = Task.objects.filter(date__month=today.month,
                                          user=self.request.user,
                                          father__isnull=True)
-        c['events'] = OrderedDict()
+        
+        # Query all events for the user, for this month
         events = Event.objects.filter(date__month=today.month,
                                       user=self.request.user)
-        (_, last_day) = calendar.monthrange(today.year, today.month)
+        
+        # I need to put them under the correct day, so I create an ordered dict, using
+        # the days of the month for as keys, and appending the events to the days
+        # when the day of the event matches 
+        c['events'] = OrderedDict()
+        (_, last_day) = calendar.monthrange(today.year, today.month) # First argument discouraged
+        
+        # Initialize the OrderedDict
         for i in range(0, last_day):
             c['events'][datetime.date(today.year, today.month, i + 1)] = []
+        
+        # Fill the dict with events
         for event in events:
             c['events'][event.date].append(event)
-        print c['events']
+        
+        # returning the context
         return c
 
 
@@ -71,6 +59,3 @@ class UpdateCompletedItemView(JSONResponseMixin, BaseUpdateView):
             'cod':1,
             'msg':'Ok'
         })
-
-        
-        
